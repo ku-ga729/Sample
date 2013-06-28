c097e0b264fcdde9ab15bf4ecc4103be6cb32310
U-ADMIN-CII6W4TFL\admin <admin@ADMIN-VGI0V2RE1.(none)>
2013-06-29 00:46:37 +0900

    certificate verify failed

--- twiRuby.rb
+++ twiRuby.rb
@@ -1,9 +1,28 @@
 require 'net/https'
 require 'base64'
 require 'uri'
-require 'config.rb'
+require './config.rb'
 
 Net::HTTP.version_1_2
 
+bearer_token = "#{CONSUMER_KEY}:#{CONSUMER_SECRET}"
+encoded_bearer_token = Base64.strict_encode64(bearer_token)
+
+url = URI.parse("https://api.twitter.com/oauth2/token")
+
+https = Net::HTTP.new(url.host, 443)
+https.use_ssl = true
+
+https.start do
+  header = {}
+  header["Authorization"] = "Basic #{encoded_bearer_token}"
+  header["Content-Type"] = "application/x-www-form-urlencoded;charset=UTF-8"
+
+  req = Net::HTTP::Post.new(url.path, header)
+  req.body = "grant_type=client_credentials"
+
+  resp = https.request(req)
+  puts resp.body
+end
 
 
